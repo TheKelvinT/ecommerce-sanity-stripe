c58c69e0b235270676d4ce96ece03a8a8bf344e9
TheKelvinT <100819164+TheKelvinT@users.noreply.github.com>
2022-12-10 16:25:32 +0800

    changed stripe variable

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,10 +1,10 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+// const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+const stripe = new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    console.log(req.body);
     try {
       const params = {
         submit_type: "pay",
@@ -41,7 +41,7 @@ export default async function handler(req, res) {
           };
         }),
         success_url: `${req.headers.origin}/success`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
+        cancel_url: `${req.headers.origin}/canceled`,
       };
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
